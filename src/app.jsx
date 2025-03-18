67ee85a55f250606b472f49f33a1585726d51230
ReactionJackson <hello@josephmjackson.com>
2025-03-18 11:42:53 +0000

    basic page shape

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,13 @@
 import { useState } from "preact/hooks";
 import styled from "styled-components";
 
+/*
+    NOTES:
+    The scout editions notebook is 30x15 squares big, plus the header info (date, title)
+*/
+
+// Styled Components:
+
 const Container = styled.div`
   display: flex;
   justify-content: center;
@@ -10,13 +17,25 @@ const Container = styled.div`
   overflow: hidden;
 `;
 
+const Page = styled.div`
+  z-index: 100;
+  position: relative;
+  width: ${({ $unit }) => $unit * 30 + 1}px;
+  height: ${({ $unit }) => $unit * 15 + 1}px;
+  border: ${({ $unit }) => $unit}px solid #fffbe8;
+  border-top-width: ${({ $unit }) => $unit * 4}px;
+  border-radius: 8px;
+  outline: 1px solid rgb(0 0 0 / 10%);
+  box-shadow: 0 5px 50px 5px rgb(0 0 0 / 5%);
+  transition: all ease 450ms;
+  box-sizing: content-box;
+`;
+
 const GridBackground = styled.div`
   z-index: -100;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
+  position: absolute;
+  inset: 0;
+  opacity: ${({ $isFaded }) => ($isFaded ? 0.25 : 1)};
   background-size: ${({ $size }) => `${$size}px ${$size}px`};
   background-image: linear-gradient(
       to right,
@@ -24,18 +43,29 @@ const GridBackground = styled.div`
       transparent 1px
     ),
     linear-gradient(to bottom, rgb(0 0 0 / 10%) 1px, transparent 1px);
-  background-position: center center;
   background-color: #fffbe8;
-  transition: background-size ease 900ms;
+  background-position: ${({ $isFaded }) => ($isFaded ? "center" : "auto")};
+  transition: background-size ease 450ms;
   pointer-events: none;
 `;
 
+// Component:
+
 export const App = () => {
+  // State:
+
   const [squareSize, setSquareSize] = useState(20);
+
+  // Render:
+
   return (
-    <Container onClick={() => setSquareSize((prev) => prev * 1.5)}>
-      writeri
-      <GridBackground $size={squareSize} />
+    <Container onClick={() => setSquareSize(squareSize === 20 ? 25 : 20)}>
+      {/* writeri */}
+      <Page $unit={squareSize}>
+        this is writeri
+        <GridBackground $size={squareSize} />
+      </Page>
+      <GridBackground $size={squareSize} $isFaded />
     </Container>
   );
 };
